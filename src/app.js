01e867d16fefd1917286d27fe13a7674378f32e5
wangshu <feiyanshiren@163.com>
2023-02-14 17:37:05 +0800

    geist ui 好像还是不够用

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 import React from 'react'
-import { useState, useEffect, useRef } from 'react'
+import { useState} from 'react'  // useEffect, useRef 
 import './app.less'
-import { GeistProvider, CssBaseline, Button, Drawer } from '@geist-ui/core'
-import { Bookmark  } from '@geist-ui/icons'
+import { GeistProvider, CssBaseline, Button, Drawer, Grid, Spacer, Tree } from '@geist-ui/core'
+import { Bookmark, Settings, PlusSquare, Star  } from '@geist-ui/icons'
 export default function App(props) {
 
   const [state, setState] = useState(false)
+
+  
+
+
+
+  const onClickTree = (path) => {
+    console.log("paht", path);
+  };
+
+  const files = [{
+    type: 'directory',
+    name: 'bin',
+    files: [{
+      type: 'file',
+      name: 'cs.js',
+    }],
+  }, {
+    type: 'directory',
+    name: 'docs',
+    files: [{
+      type: 'file',
+      name: 'controllers.md',
+    }, {
+      type: 'file',
+      name: 'es6.md',
+    }, {
+      type: 'file',
+      name: 'production.md',
+    }, {
+      type: 'file',
+      name: 'views.md',
+    }],
+  }]
+
+  const [markData, setMarkData] = useState(files)
    
     return (
       <GeistProvider>
@@ -23,12 +58,31 @@ export default function App(props) {
           <Drawer visible={state} onClose={() => setState(false)} placement="bottom"
           height={"600px"}
           >
-          {/* <Drawer.Title>标题</Drawer.Title>
-          <Drawer.Subtitle>子标题</Drawer.Subtitle> */}
+
           <Drawer.Content
           
           >
-            <p>Geist UI 是我最爱的组件库。</p>
+            <Grid.Container >
+                <Grid xs={6}>
+                  <Button  auto scale={0.5} icon={<Settings />}></Button>
+                </Grid>
+                <Grid xs={6}>
+                  <Button  auto scale={0.5} icon={<PlusSquare />}></Button>
+                </Grid>
+                <Grid xs={6}>
+                  <Button  auto scale={0.5} icon={<Star />}></Button>
+                </Grid>
+                <Grid xs={6}>
+                </Grid>
+                <Spacer h={2}/>
+                <Grid xs={24}>
+                  <Tree 
+                    value={markData}
+                    initialExpand={true}
+                    onClick={onClickTree} 
+                    />
+                </Grid>
+            </Grid.Container>
           </Drawer.Content>
           </Drawer>
         </div>
