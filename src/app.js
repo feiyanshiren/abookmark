458569923256eeca84eedd2d0bbf42076f09dc4f
wangshu <feiyanshiren@163.com>
2023-02-15 14:04:03 +0800

    使用antd再来试试

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useState} from 'react'  // useEffect, useRef 
 import './app.less'
-import { GeistProvider, CssBaseline, Button, Drawer, Grid, Spacer, Tree } from '@geist-ui/core'
-import { Bookmark, Settings, PlusSquare, Star  } from '@geist-ui/icons'
+import { BookOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 export default function App(props) {
 
   const [state, setState] = useState(false)
@@ -43,52 +43,18 @@ export default function App(props) {
   const [markData, setMarkData] = useState(files)
    
     return (
-      <GeistProvider>
-        <CssBaseline />
+      <>
 
         <div className="Book-hide">
             <Button
-              ghost auto scale={0.5}
-              icon={<Bookmark  />}
+              // type="primary"
+              icon={<BookOutlined />}
               onClick={() => setState(true)}
               >
             </Button>
         </div>
-        <div>
-          <Drawer visible={state} onClose={() => setState(false)} placement="bottom"
-          height={"600px"}
-          >
 
-          <Drawer.Content
-          
-          >
-            <Grid.Container >
-                <Grid xs={6}>
-                  <Button  auto scale={0.5} icon={<Settings />}></Button>
-                </Grid>
-                <Grid xs={6}>
-                  <Button  auto scale={0.5} icon={<PlusSquare />}></Button>
-                </Grid>
-                <Grid xs={6}>
-                  <Button  auto scale={0.5} icon={<Star />}></Button>
-                </Grid>
-                <Grid xs={6}>
-                </Grid>
-                <Spacer h={2}/>
-                <Grid xs={24}>
-                  <Tree 
-                    value={markData}
-                    initialExpand={true}
-                    onClick={onClickTree} 
-                    />
-                </Grid>
-            </Grid.Container>
-          </Drawer.Content>
-          </Drawer>
-        </div>
-
-
-      </GeistProvider>
+      </>
     )
   
 }
